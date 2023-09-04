dea6fc5f1287dd96d37ccc3853a4bdabef3d9462
Quentin PAYET <quentinpayet1203@gmail.com>
2023-09-04 10:29:05 +0400

    wip: use pytorch model for s11

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import streamlit as st
-import tensorflow as tf
-import keras
+
 import librosa
 import numpy as np
 import pandas as pd
 import joblib
-
-# import soundfile
-# import audioread
-from sklearn.preprocessing import StandardScaler as sc
 
 
 # Create a mapping from numerical values to genre names
@@ -26,31 +21,55 @@ genre_mapping = {
 }
 
 
-def audio_to_csv(audio):
+def audio_to_csv(audio, scaler):
     def audio_pipeline(audio):
         features = []
 
-        # Calcul du ZCR
-
+        # Calcul du ZCR (Zero Crossing Rate)
         zcr = librosa.zero_crossings(audio)
-        features.append(sum(zcr))
+        features.append(np.sum(zcr))  # Custom
+        features.append(np.mean(zcr))
+        features.append(np.var(zcr))
 
-        # Calcul de la moyenne du Spectral centroid
+        # Chromagram
+        chroma_stft = librosa.feature.chroma_stft(y=audio)
+        features.append(np.mean(chroma_stft))
+        features.append(np.var(chroma_stft))  # var => variance
 
-        spectral_centroids = librosa.feature.spectral_centroid(y=audio)[0]
+        # RMS (Root Mean Square value for each frame)
+        rms = librosa.feature.rms(y=audio)
+        features.append(np.mean(rms))
+        features.append(np.var(rms))
+
+        # Calcul du Spectral centroid
+        spectral_centroids = librosa.feature.spectral_centroid(y=audio)
         features.append(np.mean(spectral_centroids))
+        features.append(np.var(spectral_centroids))
+
+        # Spectral bandwith
+        spectral_bandwidth = librosa.feature.spectral_bandwidth(y=audio)
+        features.append(np.mean(spectral_bandwidth))
+        features.append(np.var(spectral_bandwidth))
 
         # Calcul du spectral rolloff point
-
         rolloff = librosa.feature.spectral_rolloff(y=audio)
         features.append(np.mean(rolloff))
+        features.append(np.var(rolloff))
+
+        # Harmonic
+        harmony = librosa.effects.harmonic(y=audio)
+        features.append(np.mean(harmony))
+        features.append(np.var(harmony))
+
+        # Tempo
+        tempo = librosa.feature.tempo(y=audio)
+        features.append(tempo[0])
 
         # Calcul des moyennes des MFCC
-
         mfcc = librosa.feature.mfcc(y=audio)
-
         for x in mfcc:
             features.append(np.mean(x))
+            features.append(np.var(x))
 
         return features
 
@@ -65,29 +84,62 @@ def audio_to_csv(audio):
 
     # Create a DataFrame
     column_names = [
-        "zcr",
-        "spectral_c",
-        "rolloff",
-        "mfcc1",
-        "mfcc2",
-        "mfcc3",
-        "mfcc4",
-        "mfcc5",
-        "mfcc6",
-        "mfcc7",
-        "mfcc8",
-        "mfcc9",
-        "mfcc10",
-        "mfcc11",
-        "mfcc12",
-        "mfcc13",
-        "mfcc14",
-        "mfcc15",
-        "mfcc16",
-        "mfcc17",
-        "mfcc18",
-        "mfcc19",
-        "mfcc20",
+        "zcr_sum",
+        "zcr_mean",
+        "zcr_var",
+        "chroma_mean",
+        "chroma_var",
+        "rms_mean",
+        "rms_var",
+        "spectral_c_mean",
+        "spectral_c_var",
+        "spectral_bandwith_mean",
+        "spectral_bandwith_var",
+        "rolloff_mean",
+        "rolloff_var",
+        "harmonic_mean",
+        "harmonic_var",
+        "tempo",
+        "mfcc1_mean",
+        "mfcc1_var",
+        "mfcc2_mean",
+        "mfcc2_var",
+        "mfcc3_mean",
+        "mfcc3_var",
+        "mfcc4_mean",
+        "mfcc4_var",
+        "mfcc5_mean",
+        "mfcc5_var",
+        "mfcc6_mean",
+        "mfcc6_var",
+        "mfcc7_mean",
+        "mfcc7_var",
+        "mfcc8_mean",
+        "mfcc8_var",
+        "mfcc9_mean",
+        "mfcc9_var",
+        "mfcc10_mean",
+        "mfcc10_var",
+        "mfcc11_mean",
+        "mfcc11_var",
+        "mfcc12_mean",
+        "mfcc12_var",
+        "mfcc13_mean",
+        "mfcc13_var",
+        "mfcc14_mean",
+        "mfcc14_var",
+        "mfcc15_mean",
+        "mfcc15_var",
+        "mfcc16_mean",
+        "mfcc16_var",
+        "mfcc17_mean",
+        "mfcc17_var",
+        "mfcc18_mean",
+        "mfcc18_var",
+        "mfcc19_mean",
+        "mfcc19_var",
+        "mfcc20_mean",
+        "mfcc20_var",
     ]
 
     df = pd.DataFrame([features], columns=column_names)
@@ -103,23 +155,27 @@ uploaded_file = st.file_uploader("Télécharger un fichier audio", type=["wav"])
 
 if uploaded_file is not None:
     # Load the trained model
-    model = keras.models.load_model("./mymodelv1.keras")
+    # ! Load pytorch model
+    # model = keras.models.load_model("./mymodelv1.keras")
 
     # Load the StandardScaler used during training
-    scaler = joblib.load("standard_scaler.pkl")  # Load the scaler from the saved file
+    scaler = joblib.load(
+        "standard_scaler_pytorch_model.pkl"
+    )  # Load the scaler from the saved file
 
     # Perform audio processing and get DataFrame and scaled features
-    df, scaled_features = audio_to_csv(uploaded_file)
+    # ! Cut the audio with 3 sec samples
+    df, scaled_features = audio_to_csv(uploaded_file, scaler)
 
     # Display the DataFrame
     st.write("Audio Features:")
     st.write(df)
 
     # Perform model prediction
-    prediction = model.predict(scaled_features)
+    # prediction = model.predict(scaled_features)
 
-    predicted_genres = [genre_mapping[i] for i in range(len(prediction[0]))]
+    # predicted_genres = [genre_mapping[i] for i in range(len(prediction[0]))]
 
     # Display prediction result
-    st.write("Prediction:")
-    st.write(pd.DataFrame([prediction[0]], columns=predicted_genres))
+    # st.write("Prediction:")
+    # st.write(pd.DataFrame([prediction[0]], columns=predicted_genres))
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Docker 
+
+`docker build -t <imageName> .`
+
+## Dev env
+`docker run -p 8501:8501 -v .:/app/ <imageName>`
