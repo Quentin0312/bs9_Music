3c3e2ec031fa1bfce5f7706e4cb24885398ac89a
Quentin PAYET <quentinpayet1203@gmail.com>
2023-09-05 11:32:04 +0400

    wip

--- modules/training.py
+++ modules/training.py
@@ -114,6 +114,7 @@ def training_loop(MusicClassifier):
 
     # Prepare data
     df = pd.read_csv("/app/resources/actual_dataset.csv")
+    # df = pd.read_csv("/app/resources/original_dataset.csv")
     X = torch.from_numpy(df.drop(columns=["label"]).to_numpy()).type(torch.float32)
     y = torch.from_numpy(df["label"].to_numpy()).type(torch.long)
 
